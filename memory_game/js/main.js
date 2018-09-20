3b113093636a7886216734f70a158b063ce81c35
Dalal SA <to7a-86@hotmail.com>
2018-09-20 06:09:18 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!");
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped "+cardsInPlay[0]);
+cardTow = cards[2];
+cardsInPlay.push(cardTow);
+console.log("User flipped "+cardsInPlay[1]);
 
-var cardOne = "queen";
-var cardTow = "queen";
-var cardThree = "king";
-var cardFour="king";
-console.log("User flipped "+cardOne);
+if (cardsInPlay.length===2){
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else
+		alert("Sorry, try again.");
+}
