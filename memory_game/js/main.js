5672ae102c70c5e5b442df4584e9141500f91b69
Dalal SA <to7a-86@hotmail.com>
2018-09-21 17:40:13 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+var userScore= 0;
 var cards = [
 {
 rank: "queen",
@@ -23,9 +24,11 @@ cardImage: "images/king-of-diamonds.png"
 
 var cardsInPlay = [];
 
+
 var checkForMatch = function(){
 	if (cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
+		userScore += 1;
+		alert("You found a match! "+"Scores: " + userScore);	
 	}
 	else{
 		alert("Sorry, try again.");
@@ -34,15 +37,40 @@ var checkForMatch = function(){
 
 
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage);
+
+
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length===2){
 		checkForMatch();
-	}
+	}	
+}
+
+var createBoard = function(){
+	resetGame();
+	
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);   
+}
+}
+
+var resetGame = function(){
+	cardsInPlay =[];
+	document.getElementById('game-board').innerHTML = "";
+    
+	document.getElementById('reset-button').addEventListener('click',createBoard);
 	
 }
-flipCard(0);
-flipCard(2);
+
+createBoard();
+
+
